16ff266f7238d8a53a74a0c310556453912061c9
Sol-Ahn <sorry0194@gmail.com>
2021-12-14 17:52:20 +0900

    fix: 불필요한 코드 삭제

--- src/components/detail/Intro.jsx
+++ src/components/detail/Intro.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import Category from "../common/Category";
 import CommonDialog from "../common/CommonDialog";
 import StyledButton from "../common/StyledButton";
 
-import sample from '../../assets/sample.png';
-import StyledButton from '../common/StyledButton';
+import sample from "../../assets/sample.png";
 
 const Intro = ({ ...props }) => {
 	const [dialogOpen, setDialogOpen] = useState(false);
@@ -53,70 +52,70 @@ const Intro = ({ ...props }) => {
 export default Intro;
 
 const IntroBox = styled.div`
-  width: 100%;
-  height: 50rem;
-  background-color: #f6f6f6;
+	width: 100%;
+	height: 50rem;
+	background-color: #f6f6f6;
 
-  display: flex;
+	display: flex;
 `;
 
 const IntroContainer = styled.div`
-  width: 104rem;
-  margin: 0 auto;
-  align-items: center;
+	width: 104rem;
+	margin: 0 auto;
+	align-items: center;
 
-  display: flex;
-  gap: 5rem;
+	display: flex;
+	gap: 5rem;
 `;
 
 const IntroThumbnail = styled.div`
-  width: 50.8rem;
-  height: 36.6rem;
+	width: 50.8rem;
+	height: 36.6rem;
 
-  img {
-    width: 100%;
-    height: 100%;
-    border-radius: 0.5rem;
-  }
+	img {
+		width: 100%;
+		height: 100%;
+		border-radius: 0.5rem;
+	}
 `;
 
 const ContentsContainer = styled.div`
-  width: 50%;
-  padding: 4rem;
+	width: 50%;
+	padding: 4rem;
 
-  display: fle column;
-  justify-content: center;
+	display: fle column;
+	justify-content: center;
 `;
 
 const Title = styled.div`
-  font-size: 3.4rem;
-  font-weight: bold;
-  margin-bottom: 1.5rem;
+	font-size: 3.4rem;
+	font-weight: bold;
+	margin-bottom: 1.5rem;
 `;
 
 const CategoryRow = styled.div`
-  display: flex;
-  gap: 1rem;
-  margin-bottom: 3rem;
+	display: flex;
+	gap: 1rem;
+	margin-bottom: 3rem;
 `;
 
 const IntroCategory = styled(Category)`
-  font-size: 1.4rem;
-  padding: 0.8rem 1.4rem;
+	font-size: 1.4rem;
+	padding: 0.8rem 1.4rem;
 `;
 
 const SubTitle = styled.div`
-  font-size: 2rem;
-  font-weight: bold;
-  color: #8f8f8f;
-  margin-bottom: 1rem;
+	font-size: 2rem;
+	font-weight: bold;
+	color: #8f8f8f;
+	margin-bottom: 1rem;
 `;
 
 const Text = styled.div`
-  font-size: 2.4rem;
-  font-weight: bold;
-  line-spacing: 0.2px;
-  margin-bottom: 2.4rem;
+	font-size: 2.4rem;
+	font-weight: bold;
+	line-spacing: 0.2px;
+	margin-bottom: 2.4rem;
 `;
 
 const ApplyButton = styled(StyledButton)`
