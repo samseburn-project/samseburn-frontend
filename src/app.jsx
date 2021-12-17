d30dde2b702a41a1d506aeb94b36faa6145e95a0
Sol-Ahn <sorry0194@gmail.com>
2021-12-18 04:39:39 +0900

    modify: 주석 처리 코드 해제

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from "./GlobalStyles";
 
-import Home from './pages/Home';
-// import Detail from './pages/Detail';
-// import RegisterPage from './components/register/RegisterPage';
-import MyPage from './pages/MyPage';
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import RegisterPage from "./components/register/RegisterPage";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
 	return (
@@ -16,7 +16,7 @@ const App = () => {
 				<Route path="/" element={<Home />} />
 				<Route path="/detail/:id" element={<Detail />} />
 				<Route path="/register" element={<RegisterPage />} />
-          <Route path="/my" element={<MyPage />} />
+				<Route path="/my" element={<MyPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
