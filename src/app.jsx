f2d02125452f8aef7eb74c3c9f759ad4f3310493
yurim <pul8219@naver.com>
2021-12-20 04:06:59 +0900

    modify: 생성페이지

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 
-import GlobalStyles from "./GlobalStyles";
+import GlobalStyles from './GlobalStyles';
 
-import Home from "./pages/Home";
-import Detail from "./pages/Detail";
-import RegisterPage from "./components/register/RegisterPage";
-import MyPage from "./pages/MyPage";
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Register from './pages/Register';
+import MyPage from './pages/MyPage';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<SnackbarProvider
-				anchorOrigin={{
-					vertical: "top",
-					horizontal: "right",
-				}}
-			>
-				<GlobalStyles />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/detail/:id" element={<Detail />} />
-					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/my" element={<MyPage />} />
-				</Routes>
-			</SnackbarProvider>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/my" element={<MyPage />} />
+        </Routes>
+      </SnackbarProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from '../components/common/Navbar';
+import Main from '../components/register/Main';
+import Footer from '../components/common/Footer';
+
+const Register = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Register;
