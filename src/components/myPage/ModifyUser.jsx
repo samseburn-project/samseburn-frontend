fa796c76ce343d9b8a26f9c1abe343058e5447ca
yurim <pul8219@naver.com>
2021-12-25 15:00:18 +0900

    modify: 닉네임 지울시 컴포넌트 사라지는 에러 해결, 라벨에 Required Mark 색상 적용

--- src/components/myPage/ModifyUser.jsx
+++ src/components/myPage/ModifyUser.jsx
@@ -1,173 +1,183 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { TextField, CircularProgress } from "@mui/material";
-
-import { ReactComponent as Delete } from "../../assets/icons/delete.svg";
-import StyledButton from "../common/StyledButton";
-import CommonDialog from "../common/CommonDialog";
+import { TextField, CircularProgress } from '@mui/material';
+import { ReactComponent as Delete } from '../../assets/icons/delete.svg';
+import StyledButton from '../common/StyledButton';
+import CommonDialog from '../common/CommonDialog';
 
 const ModifyUser = ({ userToken }) => {
-	const [loading, setLoading] = useState(true);
-	const [openDialog, setOpenDialog] = useState("");
-	const [open, setOpen] = useState(false);
-	const [nickname, setNickname] = useState("");
-	const [image, setImage] = useState({
-		imageFile: null,
-		imageUrl: null,
-	});
+  const [loading, setLoading] = useState(true);
+  const [openDialog, setOpenDialog] = useState('');
+  const [open, setOpen] = useState(false);
+  const [nickname, setNickname] = useState('');
+  const [image, setImage] = useState({
+    imageFile: null,
+    imageUrl: null,
+  });
+  const { enqueueSnackbar } = useSnackbar();
 
-	const handleOpenDialog = (targetId) => {
-		setOpenDialog(targetId);
-	};
+  const handleOpenDialog = (targetId) => {
+    setOpenDialog(targetId);
+  };
 
-	const handleOpenToggle = () => {
-		setOpen(!open);
-	};
+  const handleOpenToggle = () => {
+    setOpen(!open);
+  };
 
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
-				const res = await axios.get("https://api.samseburn.site/user", {
-					headers: { Authorization: `Bearer ${userToken}` },
-				});
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get('https://api.samseburn.site/user', {
+          headers: { Authorization: `Bearer ${userToken}` },
+        });
 
-				if (res.status === 200) {
-					setNickname(res.data.username);
-					setImage({
-						imageFile: null,
-						imageUrl: res.data.imgUrl,
-					});
-				}
-			} catch (err) {
-				console.error(err);
-			}
-		};
+        if (res.status === 200) {
+          setNickname(res.data.username);
+          setImage({
+            imageFile: null,
+            imageUrl: res.data.imgUrl,
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-		fetchUser(); // 유저 정보 요청
-		setLoading(false);
-	}, []);
+    fetchUser(); // 유저 정보 요청
+    setLoading(false);
+  }, []);
 
-	const onChangeNickname = ({ target: { value } }) => {
-		setNickname(value);
-	};
+  const onChangeNickname = ({ target: { value } }) => {
+    setNickname(value);
+  };
 
-	const setImageFromFile = ({ file, setImageUrl }) => {
-		const reader = new FileReader();
-		reader.onload = () => {
-			setImageUrl({ result: reader.result });
-		};
-		reader.readAsDataURL(file);
-	};
+  const setImageFromFile = ({ file, setImageUrl }) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImageUrl({ result: reader.result });
+    };
+    reader.readAsDataURL(file);
+  };
 
-	const onLoadFile = ({ target: { files } }) => {
-		if (files.length) {
-			setImageFromFile({
-				file: files[0],
-				setImageUrl: ({ result }) =>
-					setImage({
-						imageFile: files[0],
-						imageUrl: result,
-					}),
-			});
-		}
-	};
+  const onLoadFile = ({ target: { files } }) => {
+    if (files.length) {
+      setImageFromFile({
+        file: files[0],
+        setImageUrl: ({ result }) =>
+          setImage({
+            imageFile: files[0],
+            imageUrl: result,
+          }),
+      });
+    }
+  };
 
-	const onDeleteFile = () => {
-		setImage({
-			imageFile: null,
-			imageUrl:
-				"https://samseburn-bucket.s3.ap-northeast-2.amazonaws.com/user/SpartaIconScale7.png",
-		});
-	};
+  const onDeleteFile = () => {
+    setImage({
+      imageFile: null,
+      imageUrl:
+        'https://samseburn-bucket.s3.ap-northeast-2.amazonaws.com/user/SpartaIconScale7.png',
+    });
+  };
 
-	const onSubmitHandler = async (e) => {
-		e.preventDefault();
-		try {
-			const formData = new FormData();
-			formData.append("username", nickname);
-			formData.append(
-				"image",
-				image.imageFile ? image.imageFile : image.imageUrl
-			);
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (!nickname.trim()) {
+        enqueueSnackbar('닉네임을 입력해주세요', {
+          variant: 'warning',
+          autoHideDuration: 2000,
+        });
+        return false;
+      }
+      const formData = new FormData();
+      formData.append('username', nickname);
+      formData.append(
+        'image',
+        image.imageFile ? image.imageFile : image.imageUrl
+      );
 
-			await axios.put("https://api.samseburn.site/user", formData, {
-				headers: {
-					"Content-Type": "multipart/form-data",
-					Authorization: `Bearer ${userToken}`,
-				},
-			});
-		} catch (e) {
-			console.error(e);
-		}
-	};
+      await axios.put('https://api.samseburn.site/user', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-	if (!nickname) {
-		return null;
-	}
+  //   if (!nickname) {
+  //     return null;
+  //   }
 
-	return (
-		<>
-			{loading ? (
-				<SpinnerContainer>
-					<CircularProgress size={30} color="warning" />
-				</SpinnerContainer>
-			) : (
-				<ModifyUserBox>
-					<FormContainer>
-						<ImageContainer>
-							<ThumbnailContainer>
-								{image.imageFile || image.imageUrl ? (
-									<ImageThumbnail alt="Profile Image" src={image.imageUrl} />
-								) : (
-									<DefaultThumbnail></DefaultThumbnail>
-								)}
-								<DeleteButtonContainer onClick={onDeleteFile}>
-									<Delete alt="Delete icon" style={{ zIndex: 10 }} />
-								</DeleteButtonContainer>
-							</ThumbnailContainer>
-							<label htmlFor="input-image">
-								<ImageFileInput
-									type="file"
-									accept="image/*"
-									name="file"
-									onChange={onLoadFile}
-									id="input-image"
-								/>
-								<ImageUploadButton component="span">
-									이미지 변경
-								</ImageUploadButton>
-							</label>
-						</ImageContainer>
-						<Row>
-							<LabelText>닉네임*</LabelText>
-							<NameInput placeholder={nickname} onChange={onChangeNickname} />
-						</Row>
-						<Row>
-							<SubmitButton
-								onClick={(e) => {
-									onSubmitHandler(e);
-									handleOpenToggle();
-								}}
-							>
-								수정 완료
-							</SubmitButton>
-							<CommonDialog
-								open={open}
-								handleOpenToggle={handleOpenToggle}
-								openDialog={openDialog}
-								handleOpenDialog={handleOpenDialog}
-								mainText={"회원 정보 변경이 완료되었습니다"}
-								subText={""}
-							/>
-						</Row>
-					</FormContainer>
-				</ModifyUserBox>
-			)}
-		</>
-	);
+  return (
+    <>
+      {loading ? (
+        <SpinnerContainer>
+          <CircularProgress size={30} color="warning" />
+        </SpinnerContainer>
+      ) : (
+        <ModifyUserBox>
+          <FormContainer>
+            <ImageContainer>
+              <ThumbnailContainer>
+                {image.imageFile || image.imageUrl ? (
+                  <ImageThumbnail alt="Profile Image" src={image.imageUrl} />
+                ) : (
+                  <DefaultThumbnail></DefaultThumbnail>
+                )}
+                <DeleteButtonContainer onClick={onDeleteFile}>
+                  <Delete alt="Delete icon" style={{ zIndex: 10 }} />
+                </DeleteButtonContainer>
+              </ThumbnailContainer>
+              <label htmlFor="input-image">
+                <ImageFileInput
+                  type="file"
+                  accept="image/*"
+                  name="file"
+                  onChange={onLoadFile}
+                  id="input-image"
+                />
+                <ImageUploadButton component="span">
+                  이미지 변경
+                </ImageUploadButton>
+              </label>
+            </ImageContainer>
+            <Row>
+              <LabelText>닉네임
+			  <RequiredMark>*</RequiredMark>
+			  </LabelText>
+              <NameInput placeholder={nickname} onChange={onChangeNickname} />
+            </Row>
+            <Row>
+              <SubmitButton
+                onClick={(e) => {
+                  onSubmitHandler(e);
+                  handleOpenToggle();
+                }}
+              >
+                수정 완료
+              </SubmitButton>
+              <CommonDialog
+                open={open}
+                handleOpenToggle={handleOpenToggle}
+                openDialog={openDialog}
+                handleOpenDialog={handleOpenDialog}
+                mainText={'회원 정보 변경이 완료되었습니다'}
+                subText={''}
+              />
+            </Row>
+          </FormContainer>
+        </ModifyUserBox>
+      )}
+    </>
+  );
 };
 
 export default ModifyUser;
@@ -179,101 +189,106 @@ export default ModifyUser;
 // `;
 
 const Row = styled.div`
-	margin: 0;
+  margin: 0;
 `;
 
 const ModifyUserBox = styled.div`
-	padding-bottom: 20rem;
+  padding-bottom: 20rem;
 `;
 
 const FormContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5rem;
 `;
 
 const ImageContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ThumbnailContainer = styled.div`
-	position: relative;
+  position: relative;
 `;
 
 const ImageThumbnail = styled.img`
-	width: 24.2rem;
-	height: 24.2rem;
-	border-radius: 0.5rem;
-	object-fit: cover;
-	margin-bottom: 2rem;
+  width: 24.2rem;
+  height: 24.2rem;
+  border-radius: 0.5rem;
+  object-fit: cover;
+  margin-bottom: 2rem;
 `;
 
 const DefaultThumbnail = styled.div`
-	width: 24.2rem;
-	height: 24.2rem;
-	border-radius: 0.5rem;
-	background-color: lightgray;
-	margin-bottom: 2rem;
+  width: 24.2rem;
+  height: 24.2rem;
+  border-radius: 0.5rem;
+  background-color: lightgray;
+  margin-bottom: 2rem;
 `;
 
 const ImageFileInput = styled.input`
-	display: none;
+  display: none;
 `;
 
 const DeleteButtonContainer = styled.div`
-	position: absolute;
-	top: 1rem;
-	right: 1rem;
-	cursor: pointer;
-	z-index: 1;
-	background-color: #ffffff;
-	border-radius: 50%;
-	padding: 0.7rem;
-	opacity: 0.6;
-	transition: opacity 0.3s;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  cursor: pointer;
+  z-index: 1;
+  background-color: #ffffff;
+  border-radius: 50%;
+  padding: 0.7rem;
+  opacity: 0.6;
+  transition: opacity 0.3s;
 
-	&:hover {
-		opacity: 1;
-	}
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const ImageUploadButton = styled(StyledButton)`
-	font-size: 1.6rem;
+  font-size: 1.6rem;
 `;
 
 const LabelText = styled.div`
-	font-size: 2rem;
-	font-weight: bold;
-	margin-bottom: 2rem;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
+`;
+
+
+const RequiredMark = styled.span`
+	color: #eb3901;
 `;
 
 const SubmitButton = styled(StyledButton)`
-	font-size: 2rem;
+  font-size: 2rem;
 
-	width: 33rem;
-	height: 5rem;
+  width: 33rem;
+  height: 5rem;
 `;
 
 const NameInput = styled(TextField)`
-	width: 33rem;
-	border: 1px solid #e5e5e5;
-	border-radius: 0.5rem;
-	margin-bottom: ${(props) => (props.isNext ? "1rem" : "3rem")};
+  width: 33rem;
+  border: 1px solid #e5e5e5;
+  border-radius: 0.5rem;
+  margin-bottom: ${(props) => (props.isNext ? '1rem' : '3rem')};
 
-	.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input {
-		font-size: 1.6rem;
-		padding: 1.2rem;
-	}
+  .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input {
+    font-size: 1.6rem;
+    padding: 1.2rem;
+  }
 `;
 
 const SpinnerContainer = styled.div`
-	width: 100%;
-	height: 80vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
