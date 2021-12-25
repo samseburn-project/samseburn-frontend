81e1b97846ad712e5f509b476d5cff9c2813e4a4
Sol-Ahn <sorry0194@gmail.com>
2021-12-25 18:21:03 +0900

    style: 푸터 스타일 변경

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 const Footer = () => {
-	return <StyledFooter>Footer</StyledFooter>;
+	return (
+		<StyledFooter>
+			<Copyright>Copyright &copy; 2021 삼세번</Copyright>
+			<Text>본 사이트는 포트폴리오 용도로 제작된 사이트입니다.</Text>
+		</StyledFooter>
+	);
 };
 
 export default Footer;
@@ -9,10 +14,20 @@ export default Footer;
 const StyledFooter = styled.div`
 	width: 100%;
 	height: 8rem;
-	background-color: #c4c4c4;
+	background-color: #f6f6f6;
 	font-size: 4rem;
 
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	gap: 70rem;
+`;
+
+const Copyright = styled.div`
+	font-size: 2rem;
+	font-weight: bold;
+`;
+
+const Text = styled.div`
+	font-size: 2rem;
 `;
